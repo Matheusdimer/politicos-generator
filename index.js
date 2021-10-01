8ff8952c17dc76989e5e0f396c0d38345a367baf
matheus.dimer <matheus.dimer@betha.com.br>
2021-10-01 20:43:17 -0300

    Correção de porta da aplicação

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ const app = express();
 
 app.use('/api/politicos', politicosRoutes);
 
-app.listen(8080, () => console.log('Listening...'));
+app.listen(process.env.PORT || 8080, () => console.log('Listening...'));
